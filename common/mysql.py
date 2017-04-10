aa93ff275c352b5a6c5e82e194c1c078311d7266
lgy <liguangyu@unionpay.com>
2017-04-10 17:04:37 +0800

    工程创建。编写了common模块下的webclient和webserver

--- common/mysql.py
+++ common/mysql.py
@@ -1,1 +1,17 @@
 __author__ = 'LiGuangyu'
+
+import aiomysql
+
+
+
+class mysqlConnectionPool;
+
+    @classmethod
+    async def init(cls):
+        pool = await aiomysql.create_pool()
+
+        with await pool as conn:
+            cur = await conn.cursor()
+            await cur.execute("")
+
+
