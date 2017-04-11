c9029d6d1786b7012cdce6ab05258618eb2799d8
light <397563772@qq.com>
2017-04-11 22:10:01 +0800

    数据库自动建表

--- common/mysql.py
+++ common/mysql.py
@@ -1,33 +1,48 @@
 __author__ = 'LiGuangyu'
 
 import aiomysql
+import logging;logging.basicConfig(level=logging.INFO)
 
 from aiomysql.sa import create_engine
 from common.tableSchema import metaData,TblInsBase
 import asyncio
+
+
+from sqlalchemy import Table,Column,MetaData,Integer,String,TIMESTAMP,func
+
+metaData = MetaData()
+
+
+
 
 class dataBaseInterFace:
 
     engine=None
 
     @classmethod
-    async def init(cls):
-        cls.engine = await create_engine(
-            host='liguangyumysql.cf8iw2auduon.ap-southeast-1.rds.amazonaws.com',
-            port=3306,
-            user='gyli',
-            password='gyligyli',
-            db='CUPS_QR'
+
+    async def init(cls, dbConf=None):
+        dbConf= dict(
+            host = 'liguangyumysql.cf8iw2auduon.ap-southeast-1.rds.amazonaws.com',
+            port = 3306,
+            user = 'gyli',
+            password = 'gyligyli',
+            db = 'CUPS_QR'
         )
-        metaData.bind(cls.engine)
-        metaData.create_all(checkfirst=True)
+        cls.engine = await create_engine(**dbConf,echo=True)
+        from common.tableSchema import init
+        init(dbConf)
+
+
 
     @classmethod
     async def query(cls):
-        with await cls.engine as conn:
+        async with ( cls.engine.acquire())as conn:
             r = await conn.execute(TblInsBase.select())
             for x in r:
                 print(x)
+
+
 
 
 if __name__ == '__main__':
--- common/tableSchema.py
+++ common/tableSchema.py
@@ -1,13 +1,19 @@
 __author__ = 'LiGuangyu'
 
-from sqlalchemy import Table,Column,MetaData,Integer,String,TIMESTAMP,func
+from sqlalchemy import Table,Column,MetaData,Integer,String,TIMESTAMP,func,create_engine
 
 metaData = MetaData()
 
 TblInsBase = Table('TBL_INS_BASE',metaData,
                    Column('id',Integer,primary_key=True),
                    Column('ins_id_cd',Integer),
-                   Column('ins_en_nm',String),
+                   Column('ins_en_nm',String(200)),
                    Column('create_ts',TIMESTAMP,default=func.current_timestamp()),
                    Column('update_ts',TIMESTAMP,onupdate=func.current_timestamp()),
                    )
+
+
+def init(dbConf):
+    myEngine = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{db}'.format(**dbConf))
+    metaData.create_all(myEngine)
+
