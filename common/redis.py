6f528f74e7199d42aa260ae831774e7931258767
light <397563772@qq.com>
2017-04-12 22:45:58 +0800

    数据库自动建表

--- common/redis.py
+++ common/redis.py
@@ -1,7 +1,86 @@
 __author__ = 'LiGuangyu'
 import aioredis
+import asyncio,functools
+from concurrent.futures import CancelledError
+import logging;logging.basicConfig(level=logging.INFO)
+from common.comm import singleton
+import time
+
+def handler(loop, context):
+    print('in exception Handler')
+    print(context)
+
+class parent():
+    @classmethod
+    def exceptionDecorator(cls,func):
+        @functools.wraps(func)
+        async def wapper(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except aioredis.errors.ReplyError as err:
+                '''redis返回err信息，则进入此处'''
+                raise err
+            except CancelledError as err:
+                '''当客户端连接断开时，进入此处异常，可进行高可用处理'''
+                print('hello world')
+            except Exception as err:
+                print(type(err))
+                print(err)
+
+        return wapper
+
 
 class redis:
 
-    def __init__(self,addr='127.0.0.1',port=6379):
+    conn = None
+
+    def __init__(self):
         pass
+
+
+
+
+    @classmethod
+    async def init(cls,loop=None,addr='127.0.0.1',port=6379):
+        if not loop:
+            loop = asyncio.get_event_loop()
+        loop.set_exception_handler(handler=handler)
+        cls.conn = await aioredis.create_reconnecting_redis((addr,port),loop=loop,encoding='utf-8')
+        print('hello')
+        pass
+
+    @classmethod
+    async def incr(cls,key=None):
+        key = 'test'
+        return await cls.conn.incr(key)
+
+    @classmethod
+    async def set(cls,key,value):
+        return await cls.conn.set(key,value)
+
+    @classmethod
+    @parent.exceptionDecorator
+    async def get(cls,key):
+        return await cls.conn.get(key)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(handler=handler)
+    async def test():
+        await redis.init()
+        a = 0
+        #print(await redis.set('test',0))
+        start_time = time.time()
+        while a<100000:
+            a+=1
+            await redis.get('test')
+        print('Time Used %s' % (time.time() - start_time))
+
+    loop.run_until_complete(test())
+
--- common/tableSchema.py
+++ common/tableSchema.py
@@ -4,6 +4,7 @@ from sqlalchemy import Table,Column,MetaData,Integer,String,TIMESTAMP,func,creat
 
 metaData = MetaData()
 
+'''机构基本信息表'''
 TblInsBase = Table('TBL_INS_BASE',metaData,
                    Column('id',Integer,primary_key=True),
                    Column('ins_id_cd',Integer),
@@ -11,6 +12,9 @@ TblInsBase = Table('TBL_INS_BASE',metaData,
                    Column('create_ts',TIMESTAMP,default=func.current_timestamp()),
                    Column('update_ts',TIMESTAMP,onupdate=func.current_timestamp()),
                    )
+
+'''用户信息表'''
+
 
 
 def init(dbConf):
